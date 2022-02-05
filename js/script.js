dda2cc87232f9a35724c024afd649c02442770c9
laz <jannnjan592@gmail.com>
2022-02-05 19:30:09 +0300

    klasd

---
+++ js/script.js
@@ -0,0 +1,169 @@
+// let salom = prompt('salom');
+// let sogliq = confirm('yaxshimisan ?'); // confirm true false javob qaytaradi
+// if(sogliq){
+//     alert('yaxshi')
+// }else{
+//     alert('sogayib ket')
+// }
+
+
+// Loops => Loops can execute a block of code a number of times.
+// let i = 0 ;
+// while (i < 10) {
+// console.log('salom');
+// i++;
+// }
+// let x = 5 ;
+// x-- va x++
+
+
+// let i = 10;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 10);
+
+
+
+
+// for (let i = 0; i < 20; i++) {
+//      console.log(i);
+// }
+
+// for (let i = 20; i > 0; i--) {
+//     console.log(i);
+// }
+
+
+// let son = prompt('sonni kiriting');
+
+
+// for (let i = 0; i < son; i++) {
+//     console.log(i);
+// }
+
+
+// juft son chiqarish
+// let son = prompt('sonni kiriting');
+
+// let lazbe = 0
+
+// for (let i = 0; i < son; i++) {
+//     if(i % 2==0){
+//         console.log(i);
+//     }
+// }
+
+
+// toq son chiqarish
+// let son = prompt('sonni kiriting');
+
+// let lazbe = 0
+
+// for (let i = 0; i < son; i++) {
+//     if(i % 2 !=0){
+//         console.log(i);
+//     }
+// }   
+
+
+//     qisqa toq son chiqarish
+//     let son = prompt('sonni kiriting');
+    
+//     let lazbe = 0
+    
+//     for (let i = 0; i < son; i++) {
+//         if(i % 2 !=0){
+//             console.log(i);
+//         }
+//      }   
+
+
+
+
+
+// sonlar qatoridegi hamma sonlarni qoshish
+//      let son = +prompt('son kiriting');
+
+//      let sum = 0;
+
+//      for (let i = 1; i <= son; i++) {
+//           sum = sum + i
+//      }
+//      console.log(sum);
+
+// sonlarni qatoridegi sonlarni kopaytirish
+//     let son = prompt('son kiriting');
+
+//     let sum = 1;
+//     for (let i = 1; i < son ; i++) {
+//         sum = sum * i        
+//     }
+//     console.log(sum);
+
+
+
+// sonlar qatori orasidegi juft sonlarni kopaytpasini topish
+    // let son = prompt('son kiriting');
+    
+    // let sum = 1;
+    
+    // for (let i = 1; i <= son; i++) {
+    //     if (i % 2 == 0) {
+    //         sum = sum * i
+    //     }
+    // }
+    // console.log(sum);
+    
+    
+    
+// sonlar qatori orasidegi juft sonlarni kopaytpasini topish va qisqaroq yoli
+    // let son = prompt('son kiriting');
+    
+    // let sum = 1;
+    
+    // for (let i = 1; i <= son; i++) {
+    //     if (!(i % 2)) {
+    //         sum = sum * i
+    //     }
+    // }
+    // console.log(sum);
+    
+
+    
+// tub son oyini do while usulida
+    // let x;
+    // do {
+    //     x = +prompt('son kiriting?')
+    //     console.log(x);
+    // } while (x !=2);
+    // alert('siz 2 raqamini kiritidingiz va maglub boldingzi')
+    
+    
+// tub son oyini while usulida 
+    // let x;
+    // while (x !=2) {
+        // x = +prompt('son kiritng?')
+    // }
+    // alert('siz 2 raqamini kiritidingiz va maglub boldingzi')
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+// Uy ishi numberFirst
+    let son = prompt('son kiriting');
+    
+    for (let i = 0; i <= son; i++) {
+        
+        if (i % ) {
+            console.log(i);
+        }
+        
+    }
+    
